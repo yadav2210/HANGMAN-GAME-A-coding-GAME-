35a9ff045417b474a4cf1fdf65fb4d45433b573b
Shreya Jagatnarayan Yadav <yadavshreya2131@gmail.com>
2025-08-02 12:33:02 +0530

    first Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-center text-red "> 
+      <h1>Welcome to the React App</h1>
+    </div>
+  )
+}
+
+export default App
